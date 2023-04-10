06a64ec975c9dc4e78393ae39bc2e0f36241a84e
Ahmadillo20007 <ahmadilloqubijonov@gmail.com>
2023-04-10 22:42:47 +0500

    fwEF

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,12 +1,21 @@
 import React from 'react'
+import {  Route, Routes } from 'react-router-dom'
 import nav from '../../assents/img/nav.png'
+import Blog from '../Blog/Blog'
+import Shop from '../Shop/Shop'
 function Home() {
   return (
     <div className='Home'>
       <div className="container">
         <nav className='gold'>
 <img src={nav} alt="" />
+<ul>
+<Routes>
+  <Route path='/shop' element={<Shop/>}/>
+  <Route path='/blog' element={<Blog/>}/>
 
+</Routes>
+</ul>
         </nav>
       </div>
     </div>
