ffa4d2e6cc829fe42b045471fab4a37f16187c6e
Ahmadillo20007 <ahmadilloqubijonov@gmail.com>
2023-04-10 22:18:12 +0500

    wevew

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,13 @@
 
 import './App.css';
+import About from './pages/About/About';
+import Home from './pages/Home/Home';
 
 function App() {
   return (
     <div className="App">
-   
+   <Home/>
+   <About/>
     </div>
   );
 }
--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,11 +1,12 @@
 import React from 'react'
-import nav from '../../assents/img.nav.png'
+import nav from '../../assents/img/nav.png'
 function Home() {
   return (
     <div className='Home'>
       <div className="container">
         <nav className='gold'>
 <img src={nav} alt="" />
+
         </nav>
       </div>
     </div>
