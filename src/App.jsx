0dfce84cc6be0462d770f65216357702457ffc97
Ahmadillo20007 <ahmadilloqubijonov@gmail.com>
2023-04-11 16:48:30 +0500

    acvarv

--- src/App.jsx
+++ src/App.jsx
@@ -1,23 +1,11 @@
-import './App.css';
-<<<<<<< HEAD
-import About from './components/AboutSwiper/AboutSwipper';
-=======
-import About from './pages/About/About';
-import Home from './pages/Home/Home';
->>>>>>> 42a7c0d82ee9649165ebde71a81ff327a2ed1df2
-
+import React from 'react'
+import Home from './pages/Home/Home.jsx'
 function App() {
   return (
-    <div className="App">
-<<<<<<< HEAD
-      <About/>
-=======
-      
-   <Home/>
-   <About/>
->>>>>>> 42a7c0d82ee9649165ebde71a81ff327a2ed1df2
+    <div className='App'>
+      <Home/>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import {  Link, Outlet, Route, Routes } from 'react-router-dom'
 import nav from '../../assents/img/nav.png'
-import slider from '../../assents/img/slider.png'
+import Slider from '../../components/Slider/Slider'
 import Blog from '../Blog/Blog'
 import OurStory from '../OurStory/OurStory'
 import Shop from '../Shop/Shop'
@@ -25,7 +25,7 @@ function Home() {
           <Link className ="nav-link" to={'/blog'}>Blog</Link>
         </li>
         <li className ="nav-item">
-          <a className ="nav-link disabled" href="#" tabindex="-1" aria-disabled="true">Disabled</a>
+          <Link className ="nav-link disabled" to={'/'} tabindex="-1" aria-disabled="true">Disabled</Link>
         </li>
       </ul>
       <form className ="d-flex">
@@ -38,39 +38,15 @@ function Home() {
 <Routes>
 
   
+  <Route path='/' element={<OurStory/>}/>
   <Route path='/shop' element={<Shop/>}/>
   <Route path='/blog' element={<Blog/>}/>
-  <Route path='/OurStory' element={<OurStory/>}/>
 </Routes>
 
 <center>
-<div id="carouselExampleIndicators" className Name="carousel slide slider" data-bs-ride="carousel">
-  <div className Name="carousel-indicators">
-    <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="0" className Name="active" aria-current="true" aria-label="Slide 1"></button>
-    <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="1" aria-label="Slide 2"></button>
-    <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="2" aria-label="Slide 3"></button>
-  </div>
-  <div className Name="carousel-inner">
-    <div className Name="carousel-item active">
-  <img src={slider} className Name="d-block w-100" alt="" />
-    </div>
-    <div className Name="carousel-item">
-<img src={slider} className Name="d-block w-100" alt="" />
-    </div>
-    <div className Name="carousel-item">
-    <img src={slider} className Name="d-block w-100" alt="" />
-    </div>
-  </div>
-  <button className Name="carousel-control-prev" type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide="prev">
-    <span className Name="carousel-control-prev-icon" aria-hidden="true"></span>
-    <span className Name="visually-hidden">Previous</span>
-  </button>
-  <button className Name="carousel-control-next" type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide="next">
-    <span className Name="carousel-control-next-icon" aria-hidden="true"></span>
-    <span className Name="visually-hidden">Next</span>
-  </button>n
-</div>
+<Slider/>
 </center>
+
       </div>
     </div>
   )
